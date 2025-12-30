325e91afc56c12237e2a195908f5ca83c3547013
Hrajtocz1 <125860088+Hrajtocz1@users.noreply.github.com>
2025-12-30 20:54:37 +0100

    Refactor base48 encoding to use BigInt for precision

--- base48.js
+++ base48.js
@@ -1,4 +1,3 @@
-```javascript
 /**
  * Base-48 Encoding Standard
  * Version: 1.0.0
@@ -8,17 +7,21 @@
 const ALPHABET = "0123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjklmnpqr";
 
 export function encode(num) {
-    if (num === 0) return ALPHABET[0];
+    let n = BigInt(num); // Převod na BigInt pro nekonečnou přesnost
+    if (n === 0n) return ALPHABET[0];
+    
     let res = "";
-    while (num > 0) {
-        res = ALPHABET[num % 48] + res;
-        num = Math.floor(num / 48);
+    while (n > 0n) {
+        res = ALPHABET[Number(n % 48n)] + res;
+        n = n / 48n;
     }
     return res;
 }
 
 export function decode(str) {
     return str.split('').reduce((acc, char) => {
-        return acc * 48 + ALPHABET.indexOf(char);
-    }, 0);
+        const index = ALPHABET.indexOf(char);
+        if (index === -1) throw new Error(`Invalid character: ${char}`);
+        return acc * 48n + BigInt(index);
+    }, 0n);
 }
